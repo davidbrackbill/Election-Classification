258cfa1976db96d19b9b3bd4fc15b842e320ce2f
David Brackbill <77071769+davidbrackbill@users.noreply.github.com>
2021-12-18 17:45:02 -0800

    Add files via upload

---
+++ data/Libraries.R
@@ -0,0 +1,27 @@
+knitr::opts_chunk$set(echo = F, 
+                      message = F,
+                      warning = F,
+                      results = 'hold',
+                      fig.align = 'center',
+                      fig.height = 4, 
+                      fig.width = 4)
+
+library(pander)
+library(tidyverse)
+library(ggmap)
+library(ggthemes)
+library(gridExtra)
+library(ggridges)
+library(fmsb)
+library(magrittr)
+library(hrbrthemes)
+library(modelr)
+library(ROCR)
+library(tree)
+library(maptree)
+library(ISLR)
+library(rpart)
+library(randomForest)
+library(gbm)
+setwd('C:/Users/David/Desktop/Pstat 131/131 Project')
+
